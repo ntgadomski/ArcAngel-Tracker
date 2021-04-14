ca1a56f16b4013fe86a3c857bd6eb731c8972e4d
deleuze199 <35817671+deleuze199@users.noreply.github.com>
2021-04-14 17:17:00 -0400

    Add files via upload
    Updated GUI to show "Tail Number:", "Distance (miles):", and "Altitude (feet):"

--- aircraft_gui.py
+++ aircraft_gui.py
@@ -9,6 +9,7 @@ display_surface = pygame.display.set_mode((500, 500))
 
 tail_number = ''
 distance = ''
+altitudae = ''
 
 WHITE = (255,255,255)
 GREEN = (0,255,0)
@@ -16,10 +17,17 @@ BLUE = (0,0,128)
 
 font = pygame.font.Font('freesansbold.ttf',32)
 
-tail_number_header = font.render('TAIL:', 64, GREEN, BLUE)
+tail_number_header = font.render('Tail number:', True, GREEN, BLUE)
 tail_number_header_rect = tail_number_header.get_rect()
-tail_number_header_rect.move(25, 25)
+tail_number_header_rect.center = (100, 20)
 
+distance_header = font.render('Distance (miles):', True, GREEN, BLUE)
+distance_header_rect = distance_header.get_rect()
+distance_header_rect.center = (134, 95)
+
+altitude_header = font.render('Altitude (feet):', True, GREEN, BLUE)
+altitude_header_rect = altitude_header.get_rect()
+altitude_header_rect.center = (119, 170)
 
 while True:
     
@@ -29,7 +37,11 @@ while True:
     # to the display surface object
     # at the center coordinate.
     display_surface.blit(tail_number_header, tail_number_header_rect)
+    display_surface.blit(distance_header, distance_header_rect)
+    display_surface.blit(altitude_header, altitude_header_rect)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             sys.exit()
+            
+
     pygame.display.update()
